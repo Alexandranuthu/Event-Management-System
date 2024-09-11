934b7d807b7d707acd290e9a10e628a335fc5a75
Alexandranuthu <alexandranuthuu@gmail.com>
2024-09-11 10:27:20 +0300

    added cors options

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const express = require('express');
 const connectDB = require('./helpers/init_mongodb');
 const dotenv = require('dotenv');
 const app = express();
-
+const cors = require('cors');
 // loading the environmental variables
 dotenv.config();
+
+const corsOptions = {
+    origin: 'http://localhost:5000',
+    methods: 'GET, POST, PUT, PATCH, DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
 
 
 const authRoutes = require('./routes/authRoute');
@@ -17,6 +23,7 @@ connectDB();
 
 
 app.use(express.json()); //middleware for parsing JSON data
+app.use(cors(corsOptions)); //Use CORS middleware with specified options
 app.use('/api/auth', authRoutes); //using authRoutes
 
 // sample route to test
@@ -24,6 +31,11 @@ app.get('/', (req, res) => {
     res.send('API is running ...')
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 
 const PORT = process.env.PORT || 5000;
